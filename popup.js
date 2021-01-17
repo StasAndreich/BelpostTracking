3f42f2a11ea9ca7a27736ed011619442abee3585
Стас Медянцев <smedyancev@gmail.com>
2021-01-17 15:28:28 +0300

    Some design changes

--- popup.js
+++ popup.js
@@ -1,13 +1,15 @@
-let button = document.getElementById("trackButton");
+const button = document.getElementById("trackButton");
+const inputElement = document.getElementById("trackNumber");
+
 button.onclick = () => {
-    let inputElement = document.getElementById("trackingNumber");
     let trackNumber = inputElement.value;
-    
     if (validateTrackNumber(trackNumber))
-        open("https://belpost.by/Otsleditotpravleniye?number=" + trackNumber);
+        open("https://belpost.by/Otsleditotpravleniye?number=" 
+            + trackNumber);
 }
 
 function validateTrackNumber(number) {
     if (number.length == 13)
         return true;
+    else return false;
 }
